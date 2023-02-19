738f59805c4668acd39e67e6ff7b000cb0e3bfb9
Blazej <b.bujala@gmail.com>
2023-02-19 10:20:42 +0100

    change of task3

--- src/main/java/org/example/task3/Input.java
+++ src/main/java/org/example/task3/Input.java
@@ -11,7 +11,7 @@ public class Input {
         int numberOfAdj = scan.nextInt();
 
         for (int i = 0; i < numberOfAdj; i++) {
-            addEdge(adj, scan.nextInt(), scan.nextInt());
+            addEdges(adj, scan.nextInt(), scan.nextInt());
 
         }
         return adj;
